b493c1f770d8faf87f449ca247fa840359d2c49e
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-01-17 10:35:49 +0000

    Update index, add base route

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { connectToDb } from './config';
 import carRoutes from './routes/car';
+import { Request, Response } from 'express';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/cars', carRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+    res.json('Welcome to Christians and Hunors API');
+})
 
 const PORT = process.env.PORT || 3000;
 
